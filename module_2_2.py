3b49e3514ae9a61b3959ef29527313daf9d49ba2
KaterinaDvoriagina <ekaterima786@gmail.com>
2024-11-19 20:19:47 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,7 @@
+first = int(input('Введите число: '))
+second = int(input('Ведите число: '))
+third = int(input('Введите число: '))
+if first == second == third:
+     print(3)
+elif first== second or first== third or second== third:
+    print(2)
